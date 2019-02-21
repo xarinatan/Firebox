c0f308334e2d1485db9dfc005af1a9bf2458ce2b
Alexander IJpema <alexanderypema@gmail.com>
2019-02-21 01:43:52 +0100

    F*ck the recent-used order tab switching with a rusty rake, whoever came up with setting that as default is as mad as a fruitcake.

--- template-user.js
+++ template-user.js
@@ -1,4 +1,4 @@
 user_pref("privacy.userContext.enabled", true);
 user_pref("privacy.userContext.ui.enabled", true); 
 user_pref("lightweightThemes.selectedThemeID", "firefox-compact-dark@mozilla.org");
- 
+user_pref("browser.ctrlTab.recentlyUsedOrder", false);
