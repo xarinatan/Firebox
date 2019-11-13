7b748d6e004853efda1ad457cdbe0ed82e178876
Alexander IJpema <alexanderypema@gmail.com>
2019-11-13 16:20:19 +0100

    add some default settings that clear cache on shutdown to make firefox much more portable at the expense of a little extra bandwidth

--- template-user.js
+++ template-user.js
@@ -2,3 +2,6 @@ user_pref("privacy.userContext.enabled", true);
 user_pref("privacy.userContext.ui.enabled", true); 
 user_pref("lightweightThemes.selectedThemeID", "firefox-compact-dark@mozilla.org");
 user_pref("browser.ctrlTab.recentlyUsedOrder", false);
+user_pref("privacy.sanitize.pending", "[{"id":"shutdown","itemsToClear":["cache","offlineApps"],"options":{}}]");
+user_pref("privacy.sanitize.sanitizeOnShutdown", true);
+user_pref("privacy.history.custom", true);
